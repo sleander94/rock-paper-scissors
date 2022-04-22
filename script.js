88e85e750544f7e5e4ff4d59c665cf2c3cae4fe0
Stephen Leander <stephen.leander1@gmail.com>
2022-04-22 19:55:02 +0200

    add 5 round game function / minor edits of previous functions

--- script.js
+++ script.js
@@ -15,27 +15,53 @@ function playerPlay() {
     if (playerChoiceFormatted === 'Rock' || playerChoiceFormatted === 'Paper' || playerChoiceFormatted === 'Scissors') {
         return playerChoiceFormatted;
     } else {
-        return 'Invalid input. Please try again';
+        alert('Invalid input. Please try again');
+        playerPlay();
     }
 }
 
 /* Compare player's choice to computer's choice and determine winner */
-function playRockPaperScissors(computerSelection, playerSelection) {
+function playRound(computerSelection, playerSelection) {
     console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}`);
     if (playerSelection === computerSelection) {
         result = 'It\'s a tie!';
-    } else if (playerSelection === 'rock' & computerSelection === 'scissors') {
+        score = 0;
+    } else if (playerSelection === 'Rock' & computerSelection === 'Scissors') {
         result = 'You win! Rock beats Scissors.';
-    } else if (playerSelection === 'scissors' & computerSelection === 'paper') {
+        score = 1;
+    } else if (playerSelection === 'Scissors' & computerSelection === 'Paper') {
         result = 'You win! Scissors beats Paper.';
-    } else if (playerSelection === 'paper' & computerSelection === 'rock') {
+        score = 1;
+    } else if (playerSelection === 'Paper' & computerSelection === 'Rock') {
         result = 'You win! Rock beats Scissors.';
+        score = 1;
     } else {
-        result = `You lose! ${computerSelection} beats ${playerSelection}`
+        result = `You lose! ${computerSelection} beats ${playerSelection}`;
+        score = -1;
     }
     console.log(result);
-    return result;
+    return score;
 }
 
-
+/* Play best of 5 and record score */
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(computerPlay(), playerPlay());
+        if (score === 1) {
+            playerScore += 1;
+        } else if (score === -1) {
+            computerScore += 1;
+        }
+        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}.`);
+    }
+    if (playerScore > computerScore) {
+        console.log(`You beat the computer with a score of ${playerScore} to ${computerScore}. Congratulations!`);
+    } else if (computerScore > playerScore) {
+        console.log(`The computer beat you with a score of ${computerScore} to ${playerScore}. Better luck next time!`);
+    } else {
+        console.log(`You tied the computer with a score of ${playerScore}!`);
+    }
+}
 
