dea20ea41bb30d84d616f8208e11b92155e32811
Stephen Leander <stephen.leander1@gmail.com>
2022-04-22 19:00:54 +0200

    add player selection function

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+/* Define possible choices for player and computer */
+const gameChoices = ['rock', 'paper', 'scissors'];
 
-const gameChoices = ['rock', 'paper', 'scissors']
-
-
+/* Get computer's choice */
 function computerPlay() {
     randomNum = Math.floor(Math.random() * 3);
     computerChoice = gameChoices[randomNum];
     return computerChoice;
 }
+
+/* Get player's choice */
+function playerSelection() {
+    playerChoice = window.prompt('Rock, paper, or scissors? ').toLowerCase();
+    return playerChoice;
+}
+
+
+
+/* Compare player's choice to computer's choice and determine winner */
+
+
 
 
