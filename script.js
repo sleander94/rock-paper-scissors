c0b43faa10871fe91a0a419d6177f22ff35b00e9
Stephen Leander <stephen.leander1@gmail.com>
2022-04-23 20:43:44 +0200

    add eventlistener for buttons and refactor playround to use DOM

--- script.js
+++ script.js
@@ -22,46 +22,61 @@ function playerPlay() {
 
 /* Compare player's choice to computer's choice and determine winner */
 function playRound(computerSelection, playerSelection) {
-    console.log(`You picked ${playerSelection}. Computer picked ${computerSelection}`);
+    document.getElementById('results').innerHTML = "";
+    results = document.querySelector('#results');
+    resultDisplay = document.createElement('p');
+
+    choiceDisplay = document.createElement('p');
+    choiceDisplay.textContent = `You picked ${playerSelection}. Computer picked ${computerSelection}`;
+    results.appendChild(choiceDisplay);
+
     if (playerSelection === computerSelection) {
-        result = 'It\'s a tie!';
+        resultDisplay.textContent = 'It\'s a tie!';
         score = 0;
     } else if (playerSelection === 'Rock' & computerSelection === 'Scissors') {
-        result = 'You win! Rock beats Scissors.';
+        resultDisplay.textContent = 'You win! Rock beats Scissors.';
         score = 1;
     } else if (playerSelection === 'Scissors' & computerSelection === 'Paper') {
-        result = 'You win! Scissors beats Paper.';
+        resultDisplay.textContent = 'You win! Scissors beats Paper.';
         score = 1;
     } else if (playerSelection === 'Paper' & computerSelection === 'Rock') {
-        result = 'You win! Rock beats Scissors.';
+        resultDisplay.textContent = 'You win! Rock beats Scissors.';
         score = 1;
     } else {
-        result = `You lose! ${computerSelection} beats ${playerSelection}`;
+        resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
         score = -1;
     }
-    console.log(result);
+    results.appendChild(resultDisplay);
     return score;
 }
 
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', () => {
+    playRound(computerPlay(), `${button.classList}`);
+}));
+
+
 /* Play best of 5 and record score */
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playRound(computerPlay(), playerPlay());
-        if (score === 1) {
-            playerScore += 1;
-        } else if (score === -1) {
-            computerScore += 1;
-        }
-        console.log(`Your score: ${playerScore}. Computer score: ${computerScore}.`);
-    }
-    if (playerScore > computerScore) {
-        console.log(`You beat the computer with a score of ${playerScore} to ${computerScore}. Congratulations!`);
-    } else if (computerScore > playerScore) {
-        console.log(`The computer beat you with a score of ${computerScore} to ${playerScore}. Better luck next time!`);
-    } else {
-        console.log(`You tied the computer with a score of ${playerScore}!`);
-    }
-}
+// function game() {
+//     playerScore = 0;
+//     computerScore = 0;
+//     for (let i = 0; i < 5; i++) {
+//         playRound(computerPlay(), playerPlay());
+//         if (score === 1) {
+//             playerScore += 1;
+//         } else if (score === -1) {
+//             computerScore += 1;
+//         }
+//         console.log(`Your score: ${playerScore}. Computer score: ${computerScore}.`);
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`You beat the computer with a score of ${playerScore} to ${computerScore}. Congratulations!`);
+//     } else if (computerScore > playerScore) {
+//         console.log(`The computer beat you with a score of ${computerScore} to ${playerScore}. Better luck next time!`);
+//     } else {
+//         console.log(`You tied the computer with a score of ${playerScore}!`);
+//     }
+// }
+
 
